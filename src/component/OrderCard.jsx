e35422e029cedf5de942bd2c5ce650edf185a8fd
Daniel Asfaw <asfawdaniel779@gmail.com>
2024-03-08 01:21:05 +0300

    fix bug in ordercard

--- src/component/OrderCard.jsx
+++ src/component/OrderCard.jsx
@@ -4,13 +4,13 @@ const OrderCard = ({ title, description, price }) => {
   return (
     <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       <div className="md:flex">
-        <div className="p-8">
-          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold text-xl">
+        <div className="p-8 w-full">
+          <div className="uppercase tracking-wide text-indigo-500 font-semibold text-xl">
             {title}
           </div>
           <p className="mt-2 text-gray-500">{description}</p>
           <p className="mt-4 text-right">
-            <span className="inline-block bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            <span className="bg-green-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
               {price}
             </span>
           </p>
@@ -19,4 +19,5 @@ const OrderCard = ({ title, description, price }) => {
     </div>
   );
 };
+
 export default OrderCard;
