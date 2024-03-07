ec30a01ffba0c02725a40670b1f8f7dfa528d974
naolbm <profesornaoltena@gmail.com>
2024-03-08 01:20:50 +0300

    sth

--- src/pages/OfferDetails.jsx
+++ src/pages/OfferDetails.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom"; // Import Link from react-router-dom
 import { catagorySpecfic } from "../api";
 
+import OrderCard from "../component/OrderCard";
+
 export default function OfferDetails() {
   const location = useLocation();
   const [list, setList] = useState([]);
@@ -22,21 +24,24 @@ export default function OfferDetails() {
     fetchData();
   }, []);
 
+  //   to={`/balesuk/offer?catagoryname=${encodeURIComponent(item)}`}
   return (
     <div className="w-full bg-gray-100 min-h-[100vh]">
       <div className="w-full h-[4em] bg-white mb-[2em]"></div>
-      <div className="flex flex-col w-full justify-center items-center md:w-[40em]">
-        {list?.map((item, index) => (
-          // Wrap the entire row with a Link component
-          <Link
-            to={`/balesuk/offer?catagoryname=${encodeURIComponent(item)}`}
-            key={index}
-            className="bg-blue-100 hover:bg-blue-300 p-2 w-full h-[10em] flex rounded-[5px] items-center justify-center cursor-pointer mb-2"
-          >
-            <div className="w-[80%] text-center">{item.catagory}</div>
-          </Link>
-        ))}
-      </div>
+      {list?.map((item, index) => (
+        //   // Wrap the entire row with a Link component
+        //   <Link
+        //     to={`/balesuk/offer?catagoryname=${encodeURIComponent(item)}`}
+        //     key={index}
+        //     className=""
+        //   >
+        <OrderCard
+          title={item.product_name}
+          description={item.detail}
+          price={item.price_range}
+        />
+        //   </Link>
+      ))}
     </div>
   );
 }
